8f5380a7ce05dae26ea26cbd93303a74142761c7
vagrant <vagrant@homestead>
2019-10-12 01:18:18 +0000

    fix bug

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Mail;
 
 class UsersController extends Controller
